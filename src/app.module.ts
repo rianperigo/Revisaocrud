69193eb2c02af0b0c6ea841ccec557730ce09406
Rian Victor Santana Périgo <rianperigo@gmail.com>
2023-06-20 17:09:18 -0300

    Crud

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerMiddleware } from './users/middleware/logger.middleware';
+
+@Module({
+  imports: [UsersModule , MongooseModule.forRoot('mongodb+srv://rianperigo:rianperigo@cluster0.vv9jges.mongodb.net/')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.GET });
+  }
+}
